6b5763e3bd9f889c55be81d9f97f7fd824bba2c8
Divyam Dubey <divyamdubey2@gmail.com>
2023-06-22 10:42:38 +0530

    adjusted Scroll settings, Scroll Active Nav-links

--- js/script.js
+++ js/script.js
@@ -78,7 +78,37 @@ const sendEmail = (e) => {
 contactForm.addEventListener("submit", sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+  console.log(scrollY);
+
+  sections.forEach((current, index) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionsClass = document.querySelector(`.link${index + 1}`);
+    //   sectionsClass = document.querySelector(
+    //     ".nav__menu a[href*=" + sectionId + "]"
+    //   );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
